06d2212772bd34ad49510cc8d395b3304dd49776
Jelp200 <jorge.pena0516@gmail.com>
2025-10-13 21:27:38 -0600

    :astronaut: [ADD] ARCHIVO CONFIG

---
+++ client/src/config/error.js
@@ -0,0 +1,15 @@
+// config/error.js
+export const errorData = {
+    //* 404
+    "error": {
+        title: "Error",
+        message: "La página que buscas no existe. Intenta regresar al inicio.",
+        icon: "./assets/icons/404.svg", // Ruta del ícono para el error 404
+    },
+    //* CONSTRUCCIÓN
+    "construccion": {
+        title: "Construyendo",
+        message: "Página en desarrollo. ¡Vuelve pronto!",
+        icon: "./assets/icons/construccion.svg", // Ruta del ícono para "en construcción"
+    },
+};
---
+++ client/src/config/nosotros.js
@@ -0,0 +1,19 @@
+// config/nosotros.js
+export const nosotrosData = {
+    //* NOSOTROS
+    "informacion": {
+        title: "Nosotros",
+        textoTitulo: "¿Quiénes Somos?",
+        descripcion: "LexIA es desarrollada por IMEPOL y esta enfocada en llevar el mundo de la IA a un ambito más profesional, educativo y personal para los juristas en México",
+        imagenVertical: "/assets/backgrounds/B10.svg",
+        imagenBoton: "/assets/icons/flechasDark.svg"
+    },
+    //* QUIENES SOMOS
+    "mision": {
+        title: "Misión",
+        textoTitulo: "Nuestra Misión",
+        descripcion: "Empoderar a juristas, estudiantes y profesionales del derecho con IA que transforma datos en insights profundos, automatizan procesos complejos y personalizan el aprendizaje, elevando la justicia a la era digital.",
+        imagenVertical: "/assets/backgrounds/B11.svg",
+        imagenBoton: "/assets/icons/flechasDark.svg"
+    },
+};
