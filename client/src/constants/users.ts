a9fc5f13a49bfaf74430bf10534649881c026ecc
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-10-18 17:56:07 -0600

    [ADD] - Se agrega constantes para roles, pero no sirve el import en el script

---
+++ client/src/constants/users.ts
@@ -0,0 +1,41 @@
+// export const users = [
+//   { email: "admin@instructiva.com", password: "admin123", role: "admin", username: "Administrador", alt: "../../public/assets/imgRoles/Kirby.png" },
+//   { email: "profesor@instructiva.com", password: "profesor123", role: "profesor", username: "Profesor", alt: "../../public/assets/imgRoles/Profesor.png" },
+//   { email: "estudiante@instructiva.com", password: "estudiante123", role: "estudiante", username: "Estudiante", alt: "../../public/assets/imgRoles/Alumno.png" },
+//   { email: "pepinillo@pepinillo.com", password: "pepinilloRick123", role: "pepino", username: "Pepinillo", alt: "../../public/assets/imgRoles/Pepinillo.png" }
+// ]
+
+export const usersInstructiva = [
+	{
+		email: "admin@instructiva.com",
+		password: "admin123",
+		role: "admin",
+		username: "Administrador",
+		welcomeTitle: "Panel de Administración",
+		welcomeMessage: "Bienvenido"
+	},
+	{
+		email: "profesor@instructiva.com",
+		password: "profesor123",
+		role: "profesor",
+		username: "Profesor",
+		welcomeTitle: `Bienvenido Profesor`,
+		welcomeMessage: "Gestiona tus cursos y estudiantes"
+	},
+	{
+		email: "estudiante@instructiva.com",
+		password: "estudiante123",
+		role: "estudiante",
+		username: "Estudiante",
+		welcomeTitle: "¡Bienvenido Estudiante!",
+		welcomeMessage: "Continúa tu aprendizaje"
+	},
+	{
+		email: "pepinillo@pepinillo.com",
+		password: "pepinilloRick123",
+		role: "pepino",
+		username: "Pepinillo",
+		welcomeTitle: "Pepinillo",
+		welcomeMessage: "Me convertí en pepinillo, Morty! ¡Boom! Gran revelación: soy un pepinillo. ¿Qué te parece?"
+	}
+];
